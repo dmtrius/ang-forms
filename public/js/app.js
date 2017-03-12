459176e9f2f9ac7d14b0fc6a15fa5528f3ec4545
Dmytro Gordiienko <dmitry.gordienko@gmail.com>
2017-03-12 23:29:56 +0100

    changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,21 @@
 var app = angular.module('myApp', []);
-var FormController = function ($scope) {
+var FormController = function ($scope, $log) {
     var _this = this;
-
     this.step = 1;
 
     this.nextStep = function () {
+        $log.log('nextStep');
         _this.step++;
     };
 
     this.prevStep = function () {
+        $log.log('prevStep');
         _this.step--;
     };
 
     this.submitForm = function () {
-        console.log($scope);
         console.log("submit");
     }
 };
-FormController.$inject = ['$scope'];
+FormController.$inject = ['$scope', '$log'];
 app.controller('FormController', FormController);
